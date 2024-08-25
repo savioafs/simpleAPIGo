893dceb877772c5eba4b2e820dcc7c0ec2f3cb2e
Sávio Alexandre <savioalefreitas@gmail.com>
2024-08-25 17:34:42 -0300

    add config.go and .env && viper configures

---
+++ configs/config.go
@@ -0,0 +1,46 @@
+package configs
+
+import (
+	"github.com/go-chi/jwtauth"
+	"github.com/spf13/viper"
+)
+
+var cfg *conf
+
+type conf struct {
+	DBDriver   string `mapstructure:"DB_DRIVER"`
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBPort     string `mapstructure:"DB_PORT"`
+	DBUser     string `mapstructure:"DB_USER"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+	DBName     string `mapstructure:"DB_NAME"`
+
+	WebServerPort string `mapstructure:"WEB_SERVER_PORT"`
+
+	JWTSecret   string `mapstructure:"JWT_SECRET"`
+	JWTExpireIn int    `mapstructure:"JWT_EXPIRE_IN"`
+
+	TokenAuth *jwtauth.JWTAuth
+}
+
+func LoadConfig(path string) (*conf, error) {
+	viper.SetConfigName("app_config") // name of config file (without extension)
+	viper.SetConfigType("env")        // type of config file
+	viper.AddConfigPath(path)         // path to look for the config file in
+	viper.SetConfigFile(".env")       // config file (without extension) (name of config file)
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
+
+	return cfg, nil
+}
