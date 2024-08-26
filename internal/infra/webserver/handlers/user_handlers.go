f79c16239aedd8128034d02d41ee06d808f336e6
Sávio Alexandre <savioalefreitas@gmail.com>
2024-08-26 19:56:26 -0300

    add pattern for generate create user in swwago

--- internal/infra/webserver/handlers/user_handlers.go
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -11,6 +11,10 @@ import (
 	"github.com/savioafs/simpleAPIGo/internal/infra/database"
 )
 
+type Error struct {
+	Message string `json:"message"`
+}
+
 type UserHandler struct {
 	UserDB database.UserStorer
 }
@@ -58,23 +62,39 @@ func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+// Create user godoc
+// @Summary Create User
+// @Description Create User
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param request body dto.CreateUserInput true "user request"
+// @Success 201
+// @Failure 500 {object} Error
+// @Router /users [post]
 func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	var user dto.CreateUserInput
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 
 	u, err := entity.NewUser(user.Name, user.Email, user.Password)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 
 	err = h.UserDB.Create(u)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 
